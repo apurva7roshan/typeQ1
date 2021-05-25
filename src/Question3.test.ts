0a11ad70138d6a960ec1390e459c0703cab8f5be
apurva7roshan <51718004+apurva7roshan@users.noreply.github.com>
2021-05-25 12:38:19 +0530

    Update Question3.test.ts

--- src/Question3.test.ts
+++ src/Question3.test.ts
@@ -8,27 +8,4 @@ import {order1,orderCopy,printOrder,getPrice} from './Question3';
         }
         expect(order1).toStrictEqual(actual);
     });
-    test("ordercopy test", () => {
-        var actual = {
-            id: 1,
-            title: "Ajay",
-            price: 100,
-        }
-        expect(orderCopy).toStrictEqual(actual);
-    });
-    test("print order test", () => {
-        var actual = {
-            id: 1,
-            title: "Ajay",
-            price: 100,
-        }
-        expect(printOrder()).toStrictEqual(actual);
-    });
-    test("get price test", () => {
-        var actual = {
-            id: 1,
-            title: "Ajay",
-            price: 100,
-        }
-        expect(getPrice()).toBe(actual.price);
-    });
+ 
